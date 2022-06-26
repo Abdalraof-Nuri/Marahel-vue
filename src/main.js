6f652baeee12da9d8182a3b26e58cb343ab9c1ec
Your Name <you@example.com>
2022-06-26 19:09:36 +0200

    added a new look to the phase view with a slider and as usual everything is almonds

--- src/main.js
+++ src/main.js
@@ -5,8 +5,8 @@ import router from './router'
 import store from "./store";
 import VueCookies from 'vue-cookies'
 
-export default{
-    base_URL :'http://127.0.0.1:8000/api/'
+export default {
+    base_URL: 'http://127.0.0.1:8000/api/'
 }
 
-createApp(App).use(router).use(store).use(VueCookies, { expire: '7d'}).mount('#app')
+createApp(App).use(router).use(store).use(VueCookies, { expire: '7d' }).mount('#app')
